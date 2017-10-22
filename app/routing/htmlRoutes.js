6ee4b1676149ee408ea95120c38b2fb6fa29356b
Liz Masoudi <lizmasoudi@Lizs-MacBook-Pro-2.local>
2017-10-22 14:55:44 -0600

    routing

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,10 +1,17 @@
-var express = require("express");
-var app = express();
+// Pull in required dependencies
+var path = require('path');
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, 'home.html'));
-});
+// Export HTML routes
+module.exports = function(app) {
+	// console.log('___ENTER htmlRoutes.js___');
 
-app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, 'survey.html'));
-});
+	// Home page
+	app.get('/', function(req, res) {
+		res.sendFile(path.join(__dirname, '../public/home.html'));
+	});
+
+	// Survey page
+	app.get('/survey', function(req, res) {
+		res.sendFile(path.join(__dirname, '../public/survey.html'));
+	});
+};
