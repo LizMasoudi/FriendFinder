080f5a9daf7c62739bd6d055078aee0614fe30fd
Liz Masoudi <lizmasoudi@Lizs-MacBook-Pro-2.local>
2017-10-22 14:49:44 -0600

    routing

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-var friends = require('./app/data/friends.js');
-// var apiRoutes = require('./app/routing/apiRoutes.js');
-// var htmlRoutes = require('./app/routing/htmlRoutes');
-
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-express.static("./app/public")
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, './app/public')));
+
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
 
 
 require(path.join(__dirname, './app/routing/apiRoutes'))(app);
